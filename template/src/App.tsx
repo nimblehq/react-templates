aa8c7334b69fe581c7e85749541713d177844dbe
rafayet-monon <rafayet.monon@gmail.com>
2021-05-10 15:14:47 +0600

    [feature] Add node sass and stylelint.

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -5,13 +5,13 @@ import './App.css'
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ template/stylelint.config.js
@@ -0,0 +1,12 @@
+const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate');
+
+module.exports = {
+  extends: ['stylelint-config-sass-guidelines'],
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  rules: {
+    'order/properties-alphabetical-order': null,
+    'order/properties-order': [
+      sortOrderSmacss({ emptyLineBefore: 'always' })
+    ]
+  }
+};
