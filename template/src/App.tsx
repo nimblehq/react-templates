e55df5902a075161a78f982a1c97a83b5b62e2f6
rafayet-monon <rafayet.monon@gmail.com>
2021-05-12 17:46:40 +0600

    [feature] Added stylesheet folder structures.

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import logo from './logo.svg'
-import './App.scss'
+import logo from './assets/images/logo.svg'
+import './assets/stylesheets/app.scss'
 
 function App(): JSX.Element {
   return (
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './index.scss'
+import './assets/stylesheets/index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
