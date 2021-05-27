8dc7aff8b1dace721beea773c98200126af03ff5
Noor Ahmed <biswas.noorahmed@gmail.com>
2021-05-27 09:20:08 +0600

    Add cypress, along with basic config

--- template/cypress/integration/init.spec.js
+++ template/cypress/integration/init.spec.js
@@ -1,9 +1,9 @@
 describe('Cypress', () => {
     it('is working', () => {
-        expect(true).to.equal(true)
+      expect(true).to.equal(true)
     })
 
     it('visits the app', () => {
-        cy.visit('/')
+      cy.visit('/')
     })
 })
