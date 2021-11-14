8a09d937fac610631afad977eb76ae523d5e94f1
Xavier MALPARTY <xavier@nimblehq.co>
2021-11-14 10:48:10 +0700

    [#34] Configure i18next to manage locales

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import logo from './assets/images/logo.svg'
 import './dummy.scss'
 import './assets/stylesheets/application.scss'
 
 function App(): JSX.Element {
+  const { t } = useTranslation()
+
   return (
     <div className="app">
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {t('sample_page.edit')} <code>src/App.tsx</code> {t('sample_page.and_save_to_reload')}.
         </p>
         <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          {t('sample_page.learn_react')}
         </a>
       </header>
     </div>
