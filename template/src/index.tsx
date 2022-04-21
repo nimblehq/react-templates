70b026396aa8b4a181d95737a8399c8f92085e5d
Hoang Mirs <hoang.mirs@gmail.com>
2022-04-21 15:15:28 +0700

    [#65] Update template to use the new API in React 18

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import configureI18n from './i18n';
@@ -8,15 +9,16 @@ import reportWebVitals from './reportWebVitals';
 
 configureI18n();
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Suspense fallback="loading">
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Suspense>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
