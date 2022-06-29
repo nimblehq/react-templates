df0d9a59ce8970c4624967451d4744886cc5b595
Carryall <carryall369@gmail.com>
2022-06-29 16:13:47 +0700

    [#85] Move the inquirer to the beginning

--- packages/cli-tool/src/commands/generate/index.ts
+++ packages/cli-tool/src/commands/generate/index.ts
@@ -1,5 +1,6 @@
 import {Command, Flags, Hook} from '@oclif/core'
 import {ChildProcess} from 'node:child_process'
+import Inquirer from 'inquirer'
 import cli from 'cli-ux'
 import * as fs from 'node:fs'
 
@@ -20,23 +21,48 @@ export default class Generate extends Command {
     'version-control': Flags.string({
       char: 'c',
       description: 'version control to use in the project',
-      options: ['Github', 'Gitlab'],
+      options: ['github', 'gitlab', 'none'],
     }),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Generate)
     const appName = args.appName
+    const questions = [
+      {
+        type: 'list',
+        name: 'versionControl',
+        message: 'Select a version control service:',
+        choices: [
+          {
+            value: 'github',
+            name: 'GitHub',
+          },
+          {
+            value: 'gitlab',
+            name: 'GitLab',
+          },
+          {
+            value: 'none',
+            name: 'None',
+          },
+        ],
+      },
+    ]
+
+    let versionControl = flags['version-control']
+
+    if (!versionControl) {
+      const answers = await Inquirer.prompt(questions)
+      versionControl = answers.versionControl
+    }
 
     await this.config.runHook('initialize', {appName: appName}).then((value : Hook.Result<ChildProcess>) => {
       value.successes[0].result.on('exit', () => {
-        this.log(`Generating Nimble React app with the project name = ${appName}!`)
+        this.log(`Generating Nimble React app with the project name: ${appName}!`)
 
-        const versionControl = flags['version-control']
         if (versionControl) {
           this.setVersionControl(appName, versionControl)
-        } else {
-          this.promptVersionControl(appName)
         }
       })
     }).catch((error: string) => {
@@ -44,15 +70,13 @@ export default class Generate extends Command {
     })
   }
 
-  promptVersionControl = async (appName: string): Promise<void> => {
-    const versionControl = await cli.prompt('Which version control do you use? (Github/Gitlab)')
-    this.setVersionControl(appName, versionControl)
-  }
-
   setVersionControl = (appName: string, versionControl: string): void => {
-    if (versionControl === 'Github') {
+    if (versionControl === 'github') {
       fs.rmSync(`${appName}/.gitlab`, {recursive: true, force: true})
-    } else if (versionControl === 'Gitlab') {
+    } else if (versionControl === 'gitlab') {
+      fs.rmSync(`${appName}/.github`, {recursive: true, force: true})
+    } else {
+      fs.rmSync(`${appName}/.gitlab`, {recursive: true, force: true})
       fs.rmSync(`${appName}/.github`, {recursive: true, force: true})
     }
   }
