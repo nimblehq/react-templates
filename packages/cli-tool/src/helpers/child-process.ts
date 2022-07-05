392e40bf7c0a376377bd28cc801c596b0ab2124a
Xavier MALPARTY <xavier@nimblehq.co>
2022-07-05 15:27:22 +0700

    [#85] Fix spacing issues

--- packages/cli-tool/src/helpers/child-process.ts
+++ packages/cli-tool/src/helpers/child-process.ts
@@ -1,16 +1,21 @@
 import {spawn} from 'node:child_process'
 
-const runCommand = (name:string, options: string[]): Promise<boolean> => {
-  const initializePromise = new Promise((resolve:(value: boolean)=>void, reject) => {
-    const childProcess = spawn(name, options, {shell: true, stdio: 'inherit'})
+const runCommand = (name: string, options: string[]): Promise<boolean> => {
+  const initializePromise = new Promise(
+    (resolve: (value: boolean) => void, reject) => {
+      const childProcess = spawn(name, options, {
+        shell: true,
+        stdio: 'inherit',
+      })
 
-    childProcess.on('exit', () => {
-      resolve(true)
-    })
-    childProcess.on('error', () => {
-      reject()
-    })
-  })
+      childProcess.on('exit', () => {
+        resolve(true)
+      })
+      childProcess.on('error', () => {
+        reject()
+      })
+    },
+  )
 
   return initializePromise
 }
