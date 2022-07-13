06289d85d445e7b360b8bb4cf98f79182e197b10
Xavier MALPARTY <xavier@nimblehq.co>
2022-07-13 14:03:29 +0700

    [#104] Add test documentation and simplify testData usage

--- packages/cli-tool/test/add-ons/ui-framework/bootstrap.ts
+++ packages/cli-tool/test/add-ons/ui-framework/bootstrap.ts
@@ -1,23 +1,21 @@
 import { TestData } from '../../helpers/test-data';
 
-export const bootstrapTestData = (projectName: string): TestData => {
-  return {
-    filesShouldContain: [
-      {
-        path: `${projectName}/package.json`,
-        shouldContainString: 'bootstrap',
-      },
-      {
-        path: `${projectName}/src/assets/stylesheets/application.scss`,
-        shouldContainString: 'vendor/bootstrap',
-      },
-    ],
-    filesShouldExist: [
-      `${projectName}/src/assets/stylesheets/vendor/bootstrap/index.scss`,
-    ],
-    filesShouldNotExist: [
-      `${projectName}/tailwind.config.js`,
-      `${projectName}/src/assets/stylesheets/application.css`,
-    ],
-  };
+export const bootstrapTestData: TestData = {
+  filesShouldExist: [
+    '/src/assets/stylesheets/vendor/bootstrap/index.scss',
+  ],
+  filesShouldNotExist: [
+    '/tailwind.config.js',
+    '/src/assets/stylesheets/application.css',
+  ],
+  filesShouldContain: [
+    {
+      path: '/package.json',
+      shouldContainString: 'bootstrap',
+    },
+    {
+      path: '/src/assets/stylesheets/application.scss',
+      shouldContainString: 'vendor/bootstrap',
+    },
+  ],
 };
--- packages/cli-tool/test/helpers/test-data.ts
+++ packages/cli-tool/test/helpers/test-data.ts
@@ -1,6 +1,27 @@
+/** Describe all tests to be run for a given add-on */
 export type TestData = {
+  /** Path relative to the generated project root folder.
+   *  List files or folders that are expected to be included within
+   *  the generated project.
+   *  E.g. '/src/assets/stylesheets/application.scss`
+   */
   filesShouldExist: string[];
+  /** Path relative to the generated project root folder.
+   *  List files or folders that are expected to NOT be included within
+   *  the generated project.
+   *  E.g. '/src/assets/stylesheets/vendor/bootstrap`
+   */
   filesShouldNotExist: string[];
+  /** Path relative to the generated project root folder.
+   *  List what file is expected to contain what string.
+   *  E.g.
+   * ```
+   *   {
+   *     path: '/src/assets/stylesheets/application.scss`,
+   *     shouldContainString: 'vendor/bootstrap`,
+   *   }
+   * ```
+   */
   filesShouldContain: {
     path: string;
     shouldContainString: string;
