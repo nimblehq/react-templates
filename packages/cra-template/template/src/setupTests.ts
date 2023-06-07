553bd466660a75dc8ad0c32e686f46801366e422
Tran Manh <manh@nimblehq.co>
2023-06-07 11:52:37 +0700

    [#165] Update the import to @testing-library/react

--- packages/cra-template/template/src/setupTests.ts
+++ packages/cra-template/template/src/setupTests.ts
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { configure } from '@testing-library/dom';
+import { configure } from '@testing-library/react';
 
 configure({
   testIdAttribute: 'data-test-id',
